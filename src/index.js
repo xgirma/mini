be44e2d3e1e05c88a55831fb014bfa910e705f8d
xgirma <xgirma@gmail.com>
2018-02-20 22:32:28 -0800

    merged master

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import Application from './components/Application';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+ReactDOM.render(
+	<HashRouter>
+		<Application />
+	</HashRouter>,
+	document.getElementById('root'));
 registerServiceWorker();
