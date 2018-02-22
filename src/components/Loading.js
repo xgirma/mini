665c08aea04ac14621bb554a6a8c22b6e226082c
xgirma <xgirma@gmail.com>
2018-02-21 21:39:11 -0800

    disable loading animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -9,31 +9,31 @@ class Loading extends Component {
 	displayName = 'Loading';
 	
 	state = {
-		text: 'Loading'
+		text: 'Loading...'
 	};
 	
-	componentDidMount() {
-		let stopper = '...';
-		this.interval = window.setInterval(function() {
-			if(this.state.text === stopper){
-				this.setState(function () {
-					return {
-						text: 'Loading'
-					}
-				})
-			} else {
-				this.setState(function(previousState) {
-					return {
-						text: previousState.text + '.'
-					}
-				})
-			}
-		}.bind(this), 300)
-	}
-	
-	componentWillUnmount() {
-		window.clearInterval(this.interval);
-	}
+	// componentDidMount() {
+	// 	let stopper = '...';
+	// 	this.interval = window.setInterval(function() {
+	// 		if(this.state.text === stopper){
+	// 			this.setState(function () {
+	// 				return {
+	// 					text: 'Loading'
+	// 				}
+	// 			})
+	// 		} else {
+	// 			this.setState(function(previousState) {
+	// 				return {
+	// 					text: previousState.text + '.'
+	// 				}
+	// 			})
+	// 		}
+	// 	}.bind(this), 300)
+	// }
+	//
+	// componentWillUnmount() {
+	// 	window.clearInterval(this.interval);
+	// }
 	
 	render() {
 		return (
