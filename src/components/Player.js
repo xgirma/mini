08aa4551f7403c1216e906a500537600883c6b03
xgirma <xgirma@gmail.com>
2018-02-22 00:48:25 -0800

    add gitd to player, removed nav from player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,86 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
-import FaChevronRight from 'react-icons/lib/fa/chevron-right'
-import FaChevronLeft from 'react-icons/lib/fa/chevron-left'
-import { playlistDefault } from '../data/default';
-import { playlistType } from '../type';
+import {playlistDefault} from '../data/default';
+import {playlistType} from '../type';
 import './Player.css';
 
 class Player extends Component {
 	static propTypes = {
-	  playlist: playlistType,
-	  onPodSelection: PropTypes.func.isRequired,
-	  onAutoPlay: PropTypes.func.isRequired,
-	  autoPlay: PropTypes.bool.isRequired,
+		playlist: playlistType,
+		onPodSelection: PropTypes.func.isRequired,
+		onAutoPlay: PropTypes.func.isRequired,
+		autoPlay: PropTypes.bool.isRequired,
 	};
-
+	
 	static defaultProps = {
-	  playlist: playlistDefault,
+		playlist: playlistDefault,
 	};
-
+	
 	displayName = 'Player';
-
+	
 	state = {};
-
+	
 	handleBack = () => {
-	  const { playlist, onPodSelection, onAutoPlay } = this.props;
-	  onPodSelection(playlist[9]._id);
-	  onAutoPlay(true);
+		const {playlist, onPodSelection, onAutoPlay} = this.props;
+		onPodSelection(playlist[9]._id);
+		onAutoPlay(true);
 	};
-
+	
 	handleForward = () => {
-	  const { playlist, onPodSelection, onAutoPlay } = this.props;
-	  onPodSelection(playlist[1]._id);
-	  onAutoPlay(true);
+		const {playlist, onPodSelection, onAutoPlay} = this.props;
+		onPodSelection(playlist[1]._id);
+		onAutoPlay(true);
 	};
-
+	
 	render() {
-	  const { playlist, autoPlay } = this.props;
-	  const nowPlaying = playlist[0];
-	  const episodeTitle = nowPlaying.displayText;
-
-	  return (
-  <div className="player">
-    <div>
-      <div className="media-player">
-        <div className="navigation">
-          <div
-            onClick={this.handleBack}
-            role="link"
-            tabIndex="0"
-          >
-            <FaChevronLeft />
-          </div>
-        </div>
-        <ReactPlayer
-          url={this.props.playlist[0].url}
-          playing={autoPlay}
-          controls
-        />
-        <div className="navigation">
-          <div
-            role="link"
-            onClick={this.handleForward}
-            tabIndex="0"
-          >
-            <FaChevronRight />
-          </div>
-        </div>
-      </div>
-      <div className="social-player">
-        <p>
-          <b>
-            {episodeTitle.length > 60 ?
-									(`${episodeTitle.substring(0, 60)}...`) :
-									(episodeTitle.substring(0, 60))
-								}
-          </b>
-        </p>
-      </div>
-    </div>
-  </div>
-	  );
+		const {playlist, autoPlay} = this.props;
+		const nowPlaying = playlist[0];
+		const episodeTitle = nowPlaying.displayText;
+		
+		return (
+			<div className="container-player">
+				<div className="item-player">
+					<ReactPlayer
+						url={this.props.playlist[0].url}
+						playing={autoPlay}
+						controls
+					/>
+				</div>
+				<div className="item-title">
+					<p>
+						<b>
+							{episodeTitle.length > 60 ?
+								(`${episodeTitle.substring(0, 60)}...`) :
+								(episodeTitle.substring(0, 60))
+							}
+						</b>
+					</p>
+				</div>
+			</div>
+		);
 	}
 }
 
